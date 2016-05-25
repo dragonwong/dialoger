ac1afc83922bb2ac41ca09f5700ccc44205569f0
yayu.wang <yayu.wang@qunar.com>
2016-05-26 02:18:14 +0800

    [test] wechat sb

--- index.js
+++ index.js
@@ -1,96 +1,103 @@
-var imgInfoList = [];
+(function() {
 
-loadImgs();
+    // 为微信注入标记
+    window.wechatSb = true;
 
-$$('.checkbox').forEach(function(item) {
-    item.addEventListener('change', function(e) {
-        var index = this.parentElement.parentElement.dataset.index;
-        var checked = this.checked;
+    var imgInfoList = [];
 
-        this.nextElementSibling.disabled = !checked;
-        imgInfoList[index].checked = checked;
-    })
-});
+    loadImgs();
 
-var canvas = document.querySelector("#canvas");
-var ctx = canvas.getContext("2d");
-    
-// 点击事件
-$("#generate").addEventListener("click", function() {
-    var lastHeight = 0;
+    $$('.checkbox').forEach(function(item) {
+        item.addEventListener('change', function(e) {
+            var index = this.parentElement.parentElement.dataset.index;
+            var checked = this.checked;
 
-    for (var i = 0, len = imgInfoList.length; i < len; i++) {
-        var item = imgInfoList[i];
+            this.nextElementSibling.disabled = !checked;
+            imgInfoList[index].checked = checked;
+        })
+    });
 
-        if (item.checked) {
+    var canvas = document.querySelector("#canvas");
+    var ctx = canvas.getContext("2d");
+        
+    // 点击事件
+    $("#generate").addEventListener("click", function() {
+        var lastHeight = 0;
 
-            if (!item.loaded) {
-                alert('资源加载中，请等待或刷新重试');
-                return;
+        for (var i = 0, len = imgInfoList.length; i < len; i++) {
+            var item = imgInfoList[i];
+
+            if (item.checked) {
+
+                if (!item.loaded) {
+                    alert('资源加载中，请等待或刷新重试');
+                    return;
+                }
+
+                var img = item.img;
+                img.width = canvas.width;
+
+                var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                canvas.height = lastHeight + img.height;
+
+                ctx.putImageData(imgData, 0, 0);
+
+                // 添加图片
+                ctx.drawImage(img, 0, lastHeight, img.width, img.height);
+                
+                // 添加文字
+                var text = $$('.item .text')[i].value;
+
+                ctx.shadowOffsetX = 0;
+                ctx.shadowOffsetY = 0;
+                ctx.shadowBlur = 1;
+                ctx.shadowColor = '#000';
+                ctx.font = '22px 微软雅黑';
+                ctx.fillStyle = "#fff";
+                ctx.fillText(text, 100, 360 + lastHeight);
+
+                lastHeight = canvas.height;
             }
-
-            var img = item.img;
-            img.width = canvas.width;
-
-            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            canvas.height = lastHeight + img.height;
-
-            ctx.putImageData(imgData, 0, 0);
-
-            // 添加图片
-            ctx.drawImage(img, 0, lastHeight, img.width, img.height);
+        }
             
-            // 添加文字
-            var text = $$('.item .text')[i].value;
+        var newImg = new Image();
+        newImg.src = canvas.toDataURL('image/png');
+        $('#show').innerHTML = '';
+        $('#show').appendChild(newImg);
+    });
 
-            ctx.shadowOffsetX = 0;
-            ctx.shadowOffsetY = 0;
-            ctx.shadowBlur = 1;
-            ctx.shadowColor = '#000';
-            ctx.font = '22px 微软雅黑';
-            ctx.fillStyle = "#fff";
-            ctx.fillText(text, 100, 360 + lastHeight);
 
-            lastHeight = canvas.height;
+
+    function loadImgs() {
+        for (var i = 0; i < 4; i++) {
+            imgInfoList.push({
+                img: loadImg(i),
+                loaded: false,
+                checked: true
+            });
+        }
+
+        function loadImg(index) {
+            var img = new Image();  
+
+            img.onload = function() {
+                imgInfoList[index].loaded = true;
+            };
+            img.onerror = function(){
+                console.warm('图片:' + this.src + ' 加载失败')
+            };
+            img.src = './img/ps_' + index + '.jpg';
+
+            return img;
         }
     }
-        
-    var newImg = new Image();
-    newImg.src = canvas.toDataURL('image/png');
-    $('#show').innerHTML = '';
-    $('#show').appendChild(newImg);
-});
 
-
-
-function loadImgs() {
-    for (var i = 0; i < 4; i++) {
-        imgInfoList.push({
-            img: loadImg(i),
-            loaded: false,
-            checked: true
-        });
+    function $(selector) {
+        return document.querySelector(selector);
+    }
+    function $$(selector) {
+        return Array.prototype.slice.call(document.querySelectorAll(selector));
     }
 
-    function loadImg(index) {
-        var img = new Image();  
-
-        img.onload = function() {
-            imgInfoList[index].loaded = true;
-        };
-        img.onerror = function(){
-            console.warm('图片:' + this.src + ' 加载失败')
-        };
-        img.src = './img/ps_' + index + '.jpg';
-
-        return img;
-    }
-}
-
-function $(selector) {
-    return document.querySelector(selector);
-}
-function $$(selector) {
-    return Array.prototype.slice.call(document.querySelectorAll(selector));
-}
+})();
 
